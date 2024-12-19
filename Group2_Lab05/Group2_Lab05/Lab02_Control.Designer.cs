7bf64da2745d20e5ffe8fe447d000bd41ca58c68
Đoàn Ngọc Minh Châu ୨ৎ <167704587+23520168@users.noreply.github.com>
2024-12-19 08:23:27 +0700

    Menu lab05 (form control)

---
+++ Group2_Lab05/Group2_Lab05/Lab02_Control.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Group2_Lab05
+{
+    partial class Lab02_Control
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Lab02_Control));
+            this.btnBai05 = new System.Windows.Forms.Button();
+            this.btnBai04 = new System.Windows.Forms.Button();
+            this.btnBai03 = new System.Windows.Forms.Button();
+            this.btnBai02 = new System.Windows.Forms.Button();
+            this.btnBai01 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBai05
+            // 
+            this.btnBai05.BackColor = System.Drawing.Color.LightPink;
+            this.btnBai05.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai05.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBai05.Location = new System.Drawing.Point(1149, 444);
+            this.btnBai05.Margin = new System.Windows.Forms.Padding(6);
+            this.btnBai05.Name = "btnBai05";
+            this.btnBai05.Size = new System.Drawing.Size(247, 83);
+            this.btnBai05.TabIndex = 18;
+            this.btnBai05.Text = "Bài 05";
+            this.btnBai05.UseVisualStyleBackColor = false;
+            // 
+            // btnBai04
+            // 
+            this.btnBai04.BackColor = System.Drawing.Color.Thistle;
+            this.btnBai04.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai04.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBai04.Location = new System.Drawing.Point(872, 444);
+            this.btnBai04.Margin = new System.Windows.Forms.Padding(6);
+            this.btnBai04.Name = "btnBai04";
+            this.btnBai04.Size = new System.Drawing.Size(247, 83);
+            this.btnBai04.TabIndex = 16;
+            this.btnBai04.Text = "Bài 04";
+            this.btnBai04.UseVisualStyleBackColor = false;
+            // 
+            // btnBai03
+            // 
+            this.btnBai03.BackColor = System.Drawing.Color.LightPink;
+            this.btnBai03.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai03.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBai03.Location = new System.Drawing.Point(613, 444);
+            this.btnBai03.Margin = new System.Windows.Forms.Padding(6);
+            this.btnBai03.Name = "btnBai03";
+            this.btnBai03.Size = new System.Drawing.Size(247, 83);
+            this.btnBai03.TabIndex = 14;
+            this.btnBai03.Text = "Bài 03";
+            this.btnBai03.UseVisualStyleBackColor = false;
+            // 
+            // btnBai02
+            // 
+            this.btnBai02.BackColor = System.Drawing.Color.Thistle;
+            this.btnBai02.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai02.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBai02.Location = new System.Drawing.Point(342, 444);
+            this.btnBai02.Margin = new System.Windows.Forms.Padding(6);
+            this.btnBai02.Name = "btnBai02";
+            this.btnBai02.Size = new System.Drawing.Size(247, 83);
+            this.btnBai02.TabIndex = 15;
+            this.btnBai02.Text = "Bài 02";
+            this.btnBai02.UseVisualStyleBackColor = false;
+            // 
+            // btnBai01
+            // 
+            this.btnBai01.BackColor = System.Drawing.Color.LightPink;
+            this.btnBai01.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai01.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnBai01.Location = new System.Drawing.Point(72, 444);
+            this.btnBai01.Margin = new System.Windows.Forms.Padding(6);
+            this.btnBai01.Name = "btnBai01";
+            this.btnBai01.Size = new System.Drawing.Size(245, 83);
+            this.btnBai01.TabIndex = 13;
+            this.btnBai01.Text = "Bài 01";
+            this.btnBai01.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1562, 770);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Lab02_Control
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1562, 770);
+            this.Controls.Add(this.btnBai05);
+            this.Controls.Add(this.btnBai04);
+            this.Controls.Add(this.btnBai03);
+            this.Controls.Add(this.btnBai02);
+            this.Controls.Add(this.btnBai01);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Lab02_Control";
+            this.Text = "Menu ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBai05;
+        private System.Windows.Forms.Button btnBai04;
+        private System.Windows.Forms.Button btnBai03;
+        private System.Windows.Forms.Button btnBai02;
+        private System.Windows.Forms.Button btnBai01;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
